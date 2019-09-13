4a7735fbbc63a8bc31637bd49b00c4108726b434
Ingrid <ingrita.rpm@gmail.com>
2019-09-12 22:14:32 -0600

    Plantilla
    Plantilla con index y encabezados de cliente, administrador y mecanico

---
+++ Car-Repair/General/Administrador/Encabezado/arriba.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Car Repair</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../../Plantilla/css/reset.css" type="text/css" media="screen">
+<link rel="stylesheet" href="../../Plantilla/css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" href="../../Plantilla/css/grid.css" type="text/css" media="screen">
+<script src="../../Plantilla/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/cufon-yui.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/cufon-replace.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/Vegur_500.font.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/FF-cash.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/tms-0.3.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/tms_presets.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="../../Plantilla/js/jquery.equalheights.js" type="text/javascript"></script>
+<!-- BOOTSTRAP STYLES-->
+    <link href="../../Plantilla/assets/css/bootstrap.css" rel="stylesheet" />
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+<body id="page1">
+<div class="main-bg">
+  <div class="bg">
+    <!--==============================header=================================-->
+    <header>
+      <div class="main">
+        <div class="wrapper">
+          <h1><a href="index.html">Car Repair</a></h1>
+          <div class="fright">
+            <div class="indent"> <span class="address">Administrador</span> <span class="phone">Nombre del admin</span> </div>
+          </div>
+        </div>
+        <nav>
+          <ul class="menu">
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Maintenance </a></li>
+            <li><a href="">Repair</a></li>
+            <li><a href="">Price List</a></li>
+            <li><a href="">Locations</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!--==============================content================================-->
+    <section id="content">
+      <div class="main">
---
+++ Car-Repair/General/index.php
@@ -0,0 +1,108 @@
+<?php
+  include "Encabezado/arriba.php";
+?>
+        <div class="container_12">
+          <div class="wrapper p5">
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="../Plantilla/images/page1-img1.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Reparación del motor</h4>
+                  <p class="p2">Reparación del motor por los mejores técnicos.</p>
+                  <a class="button" href="#">Leer más</a> </div>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="../Plantilla/images/page1-img2.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Alineación de las ruedas</h4>
+                  <p class="p2">Alineación de las ruedas para todo tipo de vehiculos.</p>
+                  <a class="button" href="#">Leer más</a> </div>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="../Plantilla/images/page1-img3.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Intercambios de fluidos</h4>
+                  <p class="p2">Intercambios de fluidos</p>
+                  <a class="button" href="#">Leer más</a> </div>
+              </div>
+            </article>
+          </div>
+          <div class="container-bot">
+            <div class="container-top">
+              <div class="container">
+                <div class="wrapper">
+                  <article class="grid_8">
+                    <div class="indent-left">
+                      <h2>Bienvenido!</h2>
+                      <p class="prev-indent-bot"><strong>Car Repair</strong> es una empresa  con más de 18 años en el mercado, con técnicos especializados.</p>
+                      <p class="border-bot">Contamos con equipo moderno para darle un buen diagnostico y de ese modo evitar gastos innecesarios.  Nos comprometemos a darle calidad, rapidez y buena atención a nuestros clientes  en los servicios. </p>
+                    </div>
+                    <div class="wrapper">
+                      <div class="grid_4 alpha">
+                        <div class="indent-left">
+                          <div class="maxheight indent-bot">
+                            <h3>Acerca de nosotros</h3>
+                            <p class="prev-indent-bot"><a class="link-1" href="#">Mecánica General</a> Overhall y Medio Overhall, Tren delantero Culatas, Frenos, Servicios y más</p>
+                            <a class="link-1" href="#">Diagnóstico Electrónico</a> Escaner Actualizado, Programaciones, llaves con Chip, Reparacion de computadora y más. </div>
+                          <a class="button" href="#">Leer más</a> </div>
+                      </div>
+                      <div class="grid_4 omega">
+                        <div class="indent-left2">
+                          <div class="maxheight indent-bot">
+                            <h3 class="p0">Nuestros servicios</h3>
+                            <ul class="list-1">
+                              <li><a href="#">Diagnóstico completo de computadora</a></li>
+                              <li><a href="#">Análisis completo de seguridad</a></li>
+                              <li><a href="#"></a>Problemas de conducción</li>
+                              <li><a href="#">Cambios de aceite</a></li>
+                              <li><a href="#"></a>Instalación de reparación de emisiones</li>
+                              <li><a href="#">Servicio de Aire Acondicionado</a></li>
+                              <li><a href="#">Sistemas Eléctricos</a></li>
+                              <li><a href="#"></a>Mantenimiento de la flota</li>
+                            </ul>
+                          </div>
+                          <a class="button" href="#">Leer más</a> </div>
+                      </div>
+                    </div>
+                  </article>
+                  <article class="grid_4">
+                    <div class="indent-left2 indent-top">
+                      <div class="box p4">
+                        <div class="padding">
+                          <div class="wrapper">
+                            <figure class="img-indent"><img src="../Plantilla/images/page1-img4.png" alt=""></figure>
+                            <div class="extra-wrap">
+                              <h3 class="p0">Horario:</h3>
+                            </div>
+                          </div>
+                          <p class="p1"><strong>24 Hour Emergency Towing</strong></p>
+                          <p class="color-1 p0">Lunes - Viernes: 7:30 am - 6:00</p>
+                          <p class="color-1 p1">Sabado: 7:30 am - Noon</p>
+                          Night Drop Available </div>
+                      </div>
+                      <figure class="indent-bot">
+                        <iframe width="260" height="202" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+                      </figure>
+                      <div class="indent-left">
+                        <dl class="main-address">
+                          <dt>Demolink.org 8901 Marmora Road,<br>
+                            Glasgow, D04 89GR.</dt>
+                          <dd><span>Telephone:</span> +502 2256 5683;</dd>
+                          <dd><span>FAX:</span> +502 2659 2656</dd>
+                          <dd><span>E-mail:</span><a href="#">mail@carrepair.com</a></dd>
+                        </dl>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+<?php
+  include "Encabezado/abajo.php";
+?>
