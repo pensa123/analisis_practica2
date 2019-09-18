6ee8617eccf625957efc42ea24d625bc459a0d71
Fernando Pensamiento <fernanpensa@gmail.com>
2019-09-17 20:18:31 -0600

    Agregando prueba eliminar vehiculo

--- Car-Repair/General/BaseDatos/Vehiculos.php
+++ Car-Repair/General/BaseDatos/Vehiculos.php
@@ -2,6 +2,16 @@
 include_once("Consultas.php");
 
 
+function eliminarVehiculo($placa){
+    try{
+        $var = sprintf("DELETE from vehiculo where placa = '%s'; " , $placa);
+        echo $var;
+        query($var);
+        return true; 
+    }catch(Exception $e){}
+
+}
+
 function crearVehiculo($placa, $marca, $modelo , $aniofab){
     try{
         $var = sprintf("INSERT INTO vehiculo values('%s' , '%s', '%s' , %d);", $placa , $marca , $modelo , $aniofab); 
--- Car-Repair/General/Pruebas/Vehiculos/pruebaVehiculo.php
+++ Car-Repair/General/Pruebas/Vehiculos/pruebaVehiculo.php
@@ -2,19 +2,25 @@
 
 
 use PHPUnit\Framework\TestCase;
-
+include_once(__DIR__ . "\..\..\BaseDatos\Vehiculos.php");
+        
 class PruebaVehiculo extends TestCase
 //class PruebaVehiculo extends PHPUnit_Framework_TestCase
 {
+    
     public function CorrerTodo()
     { 
+        $this->testElimVehiculo();
         $this->testCrearVehiculo();
+    }
+
+    public function testElimVehiculo(){
+
+        $this->assertNotEmpty(eliminarVehiculo(('p055chk')));
     }
 
     public function testCrearVehiculo()
     {
-        include_once(__DIR__ . "\..\..\BaseDatos\Vehiculos.php");
-        
         $this->assertNotEmpty(crearVehiculo("p055chk", "kia" , "pop" , 1999) );
     }
 }
