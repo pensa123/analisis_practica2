c280e736bfd8763d5e76929b82e71f5afce69797
Ingrid <ingrita.rpm@gmail.com>
2019-09-18 16:59:27 -0600

    Prueba update mecanico

--- Car-Repair/General/Pruebas/Mecanicos/PruebaMecanicos.php
+++ Car-Repair/General/Pruebas/Mecanicos/PruebaMecanicos.php
@@ -10,7 +10,7 @@ class PruebaMecanicos extends PHPUnit_Framework_TestCase
   }
   public function testMecanicos(){
     //Eliminar mecanico pero no existe
-    $this->assertSame(true, eliminar_mecanico(2658762580403));
+    $this->assertSame(true, eliminar_mecanico(30));
     //crear
     $this->assertSame(true,crear_mecanico(30,"Treinta","tt","1234","dire","56565","ttnick","1234"));
     //eliminar, si existe
@@ -22,7 +22,7 @@ class PruebaMecanicos extends PHPUnit_Framework_TestCase
     $this->assertSame(false,crear_mecanico(null,"Treinta","tt","1234","dire","56565","ttnick","1234"));
     $this->assertSame(false,crear_mecanico("hola","Treinta","tt","1234","dire","56565","ttnick","1234"));
     $this->assertSame(false,actualizar_mecanico(30,null,"cero","cero","0","cero","00","2019-02-01",1,"cero","1234"));
-    $this->assertSame(true,actualizar_mecanico(30,"null","cero","cero","0","cero","00","2019-02-01",1,"cero","1234"));
+    $this->assertSame(false,actualizar_mecanico(30,"cadena","el nulo","cero","0","cero","00","2019-02-01",1,"cero","1234"));
 
   }
 }
