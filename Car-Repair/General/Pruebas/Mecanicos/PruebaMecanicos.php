eab4972fc3aa7c8dbbeba5649548b2d5d63aa9a1
Ingrid <ingrita.rpm@gmail.com>
2019-09-18 01:14:49 -0600

    Prueba mecanicos

--- Car-Repair/General/Pruebas/Mecanicos/PruebaMecanicos.php
+++ Car-Repair/General/Pruebas/Mecanicos/PruebaMecanicos.php
@@ -8,7 +8,6 @@ class PruebaMecanicos extends TestCase
     $this->testMecanicos();
   }
   public function testMecanicos(){
-<<<<<<< HEAD
     //prueba crear mecánico
     echo "\nEliminar si existe";
     $this->assertSame(true, eliminar_mecanico(2658762580403));
@@ -25,11 +24,7 @@ class PruebaMecanicos extends TestCase
     $this->assertSame(false,crear_mecanico(null,"Treinta","tt","1234","dire","56565","ttnick","1234"));
     echo "\nActualizar";
     $this->assertSame(false,actualizar_mecanico(30,null,"cero","cero","0","cero","00","2019-02-01",1,"cero","1234"));
-    //prueba eliminar mecánico
-=======
-    //prueba crear mecanico
-    $this->assertEquals("\"alert alert-danger\"", create_mecanico('3016252620302','Mecanico prueba','mecanico_prueba@correo.com','12653265','Direccion de prueba','77659563','mec_prue','1234'));
->>>>>>> f3.0-201602420
+
   }
 }
 
